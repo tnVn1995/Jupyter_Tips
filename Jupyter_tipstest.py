f4502e4671d06d363048fbd82a805cf3a0bbf65a
tung2921 <35050694+tung2921@users.noreply.github.com>
2020-03-24 16:10:33 -0500

    fixed untracked files

---
+++ Jupyter_tipstest.py
@@ -0,0 +1,22 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.4.1
+#   kernelspec:
+#     display_name: 'Python 3.7.6 64-bit (''DL'': conda)'
+#     language: python
+#     name: python37664bitdlconda6fa35517b7c749c08daae4e0143549ab
+# ---
+
+import numpy as np
+import matplotlib.pyplot as plt
+x = np.arange(-3, 3, 0.1)
+y = np.cos(x)
+plt.plot(x, y)
+plt.show()
+
+
